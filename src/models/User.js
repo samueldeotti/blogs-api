09913274f7645751b2e50abba3c37444ba6c054c
Samuel Deotti <samuel_deotti@hotmail.com>
2023-12-26 13:22:14 -0300

    req02

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
