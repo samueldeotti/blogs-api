29b4ecb03309ba2670f8bc9f7ba41fc370218b33
Samuel Deotti <samuel_deotti@hotmail.com>
2023-12-26 16:27:54 -0300

    req11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+const PostModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, {
+    tableName: 'blog_posts',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogpost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    }); 
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    }); 
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostModel;
