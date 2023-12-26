4955ca71832fdf75f9323574d8f1666d327847e7
Samuel Deotti <samuel_deotti@hotmail.com>
2023-12-26 16:23:42 -0300

    req10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,36 @@
+const BlogModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+    
+  }, {
+    tableName: 'blog_posts',
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  }
+
+  return BlogPost;
+};
+
+module.exports = BlogModel;
--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,13 @@ const UserModel = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'blogPosts'
+    })
+  }
+
   return User;
 };
 
